040795e368b243f898441176750da7dd5fe55580
Balazs <balazs@frog.net>
2022-06-10 12:16:58 +0100

    Initial Commit

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1>Jokes</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: 'Welcome to Dadjokes',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+     <h1>About Dadjokes</h1>
+     <p>This is an app that desplays corny dad jokes</p>
+    </div>
+</template>
+
+<script>
+export default {
+    head(){
+        return {
+            title: 'About The App',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'best place for corny dad jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
